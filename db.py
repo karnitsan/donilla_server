b34c79c90df70fceeb8c34f8f66e19aee426fc15
karnitsan <s_s_c@inter.net.il>
2018-06-13 20:11:58 +0300

    Update db.py

--- db.py
+++ db.py
@@ -8,11 +8,14 @@ DB_NAME = 'donilla.db'
 
 
 class UnknownUser(Exception):
-    'Unknown user ID.'
+    'Unknown user mail.'
 
 
 class DuplicateUser(Exception):
-    'Duplicate user ID.'
+    'Duplicate user mail.'
+
+class WrongUserPassword(Exception):
+    'Wornd user password.'
 
 
 @contextlib.contextmanager
@@ -38,32 +41,130 @@ def init_db():
         if len(sql.fetchall()) == 1:
             LOGGER.debug('database already exists')
             return
+
+        sql.execute('''
+            CREATE TABLE campaigns(
+                id VARCHAR(32) UNIQUE,
+                name VARCHAR(256),
+                amota VARCHAR(256),
+                status INTEGER,
+                target INTEGER,
+                start_date VARCHAR(32),
+                end_date VARCHAR(32),
+                clearing_info VARCHAR(256),
+                link_home_page VARCHAR(256),
+                link_logo VARCHAR(256),
+                link_text VARCHAR(256),
+                link_video VARCHAR(256),
+                link_pic1 VARCHAR(256),
+                link_pic2 VARCHAR(256)
+                )''')
+        LOGGER.debug('campign table created')
+
         sql.execute('''
             CREATE TABLE users(
-                user_id VARCHAR(32) UNIQUE,
-                full_name VARCHAR(256),
-                kwargs VARCHAR(1024))''')
+                mail VARCHAR(256) UNIQUE,
+                password VARCHAR(256),
+                first_name VARCHAR(32),
+                last_name VARCHAR(32),
+                nick_name VARCHAR(32),
+                gender INTEGER,
+                address VARCHAR(256),
+                id_number VARCHAR(32),
+                age_verifyed BOOLEAN,
+                mail_verifyed BOOLEAN,
+                status INTEGER
+                )''')
         LOGGER.debug('user table created')
+        
+        sql.execute('''
+            CREATE TABLE donations(
+                id VARCHAR(32) UNIQUE,
+                campaign_id VARCHAR(32),
+                mail VARCHAR(256),
+                amount INTEGER,
+                totel INTEGER,
+                date VARCHAR(32),
+                time VARCHAR(32),
+                voucher_info VARCHAR(256),
+                approval_info VARCHAR(256)
+                )''')
+        LOGGER.debug('donate table created')
+          
+        sql.execute('''
+            CREATE TABLE lottery_game(
+                id VARCHAR(32) UNIQUE,
+                status INTEGER,                
+                cycle INTEGER,
+                winner_index INTEGER,
+                date VARCHAR(32),
+                time VARCHAR(32)
+                )''')
+        LOGGER.debug('lottery_game table created')
+        
+        sql.execute('''
+            CREATE TABLE lottery_ticket(
+                id VARCHAR(32) UNIQUE,
+                mail VARCHAR(256),
+                lottery_game_id VARCHAR(32),
+                price INTEGER,
+                luck_number INTEGER,
+                ticket_index INTEGER,
+                total INTEGER,
+                date VARCHAR(32),
+                time VARCHAR(32)
+                )''')
+        LOGGER.debug('lottery_ticket table created')
 
+        sql.execute('''
+            CREATE TABLE spinner_game(
+                id VARCHAR(32) UNIQUE,
+                status INTEGER,
+                cycle INTEGER,
+                date VARCHAR(32),
+                time VARCHAR(32)
+                )''')
+        LOGGER.debug('spinner_game table created')
+                
+        sql.execute('''
+            CREATE TABLE spinner_ticket(
+                id VARCHAR(32) UNIQUE,
+                mail VARCHAR(256),
+                spinner_game_id VARCHAR(32),
+                price INTEGER,
+                ticket_index INTEGER,
+                spinner_slot INTEGER,
+                total INTEGER,
+                date VARCHAR(32),
+                time VARCHAR(32)
+                )''')
+        LOGGER.debug('spinner_ticket table created')
+         
 
-def get_user(user_id):
+def get_user(user_mail, user_password):
     'Get details of a user.'
     with sql_connection() as sql:
-        sql.execute("SELECT * FROM users WHERE user_id = ?", (user_id,))
+        sql.execute("SELECT * FROM users WHERE mail = ?", (user_mail,))
         try:
             user_details = sql.fetchall()[0]
         except IndexError:
-            raise UnknownUser("user {} does not exist".format(user_id))
+            raise UnknownUser("user {} does not exist".format(user_mail))
+        
+        p = user_details["password"]
+        if p != user_password:
+            raise WrongUserPassword("user {} Wrong user password".format(user_mail))
         return {key: user_details[key] for key in user_details.keys()}
 
 
-def add_user(user_id, full_name):
-    'Add a user to the database.'
+def add_user(user_mail, user_password, user_nick_name):
+    'Add a user to the database. '
     with sql_connection() as sql:
         try:
-            sql.execute("INSERT INTO users (user_id, full_name) VALUES (?, ?)", (user_id, full_name))
+            sql.execute("INSERT INTO users (mail, password, nick_name) VALUES (?, ?, ?)", (user_mail, user_password, user_nick_name))
         except sqlite3.IntegrityError as exception:
             bad_column_name = str(exception).split('.')[-1]
             bad_value = locals().get(bad_column_name)
             raise DuplicateUser("{} {} is non unique".format(bad_column_name, bad_value))
-    return get_user(user_id)
+    return get_user(user_mail, user_password)
+
+#init_db()
